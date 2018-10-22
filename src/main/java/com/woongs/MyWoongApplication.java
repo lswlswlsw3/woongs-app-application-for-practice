f4c1d8b872f0996370642ad6c878c6f7340e8f60
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-10-22 11:17:35 +0900

    스프링부트, 첫 페이지 작성후 커밋

---
+++ src/main/java/com/woongs/MyWoongApplication.java
@@ -0,0 +1,12 @@
+package com.woongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWoongApplication.class, args);
+	}
+}
---
+++ src/test/java/com/woongs/MyWoongApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woongs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyWoongApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
