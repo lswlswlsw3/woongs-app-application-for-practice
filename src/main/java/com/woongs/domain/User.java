4349b3324bb16fe24254f78f31f4f1ab88f5d482
DESKTOP-2FMNJGO\Woongs <lswlswlsw3@gmail.com>
2018-10-23 13:47:40 +0900

    URL, HTML 리펙토링, 사용자 정보 수정 기능 추가 함.

--- src/main/java/com/woongs/domain/User.java
+++ src/main/java/com/woongs/domain/User.java
@@ -1,7 +1,19 @@
-package com.woongs.web;
+package com.woongs.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=20)
 	private String userId;
+	
 	private String password;
 	private String name;
 	private String email;
@@ -25,5 +37,11 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
 	}
 }
--- src/main/java/com/woongs/web/UserController.java
+++ src/main/java/com/woongs/web/UserController.java
@@ -1,27 +1,55 @@
 package com.woongs.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.woongs.domain.User;
+import com.woongs.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println("user : "+user);
-		users.add(user);
-		return "redirect:/list";
+	@Autowired
+	private UserRepository UserRepository;
+
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
 	}
 	
-	@GetMapping("/list")
+	@PostMapping("")
+	public String create(User user) {
+		System.out.println("user create come! "+user);
+		UserRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		System.out.println("user list come!");
+		model.addAttribute("users", UserRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		System.out.println("id/form come! "+UserRepository.findById(id).get());
+		model.addAttribute("user", UserRepository.findById(id).get()); // ID에 따른 정보 조회후, model에 담음
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = UserRepository.findById(id).get();
+		user.update(newUser);
+		UserRepository.save(user);
+		return "redirect:/users";
 	}
 }
